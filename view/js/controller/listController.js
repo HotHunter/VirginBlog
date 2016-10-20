ba3a3822c6d74b1fdc6d758e6658b68b6f1802f7
HotHunter <449757893@qq.com>
2016-10-20 17:35:07 +0800

    'test'

--- view/js/controller/listController.js
+++ view/js/controller/listController.js
@@ -2,33 +2,25 @@
 * @Author: [GHT] <odin.guott@gmail.com>
 * @Date:   2016-10-17 19:50:34
 * @Last Modified by:   [GHT] <odin.guott@gmail.com>
-* @Last Modified time: 2016-10-17 21:21:48
+* @Last Modified time: 2016-10-18 11:09:00
 */
 
 ;(function($){
 
 $(window).load(function() {
-    var url = "";//求求后台的url
-    var param = {};
-    param.pageNo = 2;
-    param.pageSize = 10;
-   $.ajax({
-       type: 'GET',//GET/POST
-       url: url + "?data=" + JSON.stringify(param),
-       success: function (data, textStatus) {
-
-       }
-});
-
+  var pageno = 1,
+      pagesize = 10,
+      url = '';
+  listAjax(url,pageno,pagesize);
 })(jQuery);
 
-var data = [
-  {title:"test1",info:"黄",content:"第一篇文章内容"},
-  {title:"test2",info:"郭",content:"第二篇文章内容"},
-  ];
+// var data = [
+//   {title:"test1",info:"黄",content:"第一篇文章内容"},
+//   {title:"test2",info:"郭",content:"第二篇文章内容"},
+//   ];
 
-  function initList(){
-  alert('start');
+//收到返回数据后初始化文章列表
+function initList(data){
       for (var i = 0; len = data.length, i < len; i++) {
       var article = 
           "<article><div id='ar_title'>"+data[i].title+"</div><div id='ar_info'>"+data[i].info+"</div><div id='ar_content'>"+data[i].content+"</div></article>";
@@ -36,3 +28,23 @@ var data = [
       $("#list_body").append(article);
     }
 }
+
+//定义公共ajax方法
+function listAjax(url,pageno,pagesize){
+  var param = {};
+  param.pageno = pageno;
+  param.pagesize = pagesize;
+  $.ajax({
+    type: 'POST',
+    url : url,
+    data  : json.stringify(data),
+    dateType : 'json'
+    success: function(data , textStatus){
+      initList(data);
+    }
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      alert("XMLHttpRequest:" + XMLHttpRequest + ",textStatus:" + textStatus + ",errorThrown:" + errorThrown)
+    }
+  })
+}
+
